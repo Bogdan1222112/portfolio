f5229ef47a6f5d59ccb13e69d25b35a940f70fd2
Bogdan Yarovenko <156672634+Bogdan1222112@users.noreply.github.com>
2024-05-30 12:24:28 +0000

    mobile version

--- js/main.js
+++ js/main.js
@@ -15,16 +15,25 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
 const btnDarkMode = document.querySelector('.dark-mode-btn');
+const header = document.querySelector('header');
+const body = document.body; 
+
 btnDarkMode.onclick = function() {
     btnDarkMode.classList.toggle('dark-mode-btn--active');
-   const isLightTheme  =  document.body.classList.toggle('light-theme');
+    const isLightTheme = body.classList.toggle('light-theme');
+    
+    // Toggle background image for the header
+    header.style.backgroundImage = isLightTheme 
+        ? "url('img/header-bg-light.png')" 
+        : "url('img/header-bg-dark.png')";
+    
+    // Update icon visibility based on theme
     document.querySelectorAll('.dark-icon').forEach(icon => {
         icon.style.display = isLightTheme ? 'none' : 'inline';
     });
     document.querySelectorAll('.light-icon').forEach(icon => {
         icon.style.display = isLightTheme ? 'inline' : 'none';
     });
-    header.style.backgroundImage = isLightTheme ? "url('../img/header-bg-dark.png')" : "url('../img/header-bg-light.png')";
-}
-
+};
